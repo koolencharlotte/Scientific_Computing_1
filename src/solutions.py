76b0eea325c8216e6290edaff76804c5c33a529a
chrishoynck <94909944+chrishoynck@users.noreply.github.com>
2025-02-11 11:07:10 +0100

    finished up part 1

--- src/solutions.py
+++ src/solutions.py
@@ -1,13 +1,12 @@
 import numpy as np 
 
-def spat_approx_1a(deltax, time, solutions, x, L):
+def spat_approx_1a(deltax, solutions):
     return (solutions[2] - 2*solutions[1] + solutions[0])/np.power(deltax, 2)
 
-def time_approx_1a(deltat, time, func, x):
-    return (func(x, time+deltat) - 2*func(x, time) + func(x, time-deltat))/np.power(deltat, 2)
+# def time_approx_1a(deltat, time, func, x):
+#     return (func(x, time+deltat) - 2*func(x, time) + func(x, time-deltat))/np.power(deltat, 2)
 
-def one_b(which_one):
-
+def initialize_wave(which_one, L, N):
     def b_one(x):
         return np.sin(2*np.pi*x)
 
@@ -20,13 +19,7 @@ def one_b(which_one):
         else:
             return 0
 
-    L = 1
-    N = 100
-    c=1
-    time = 0
-
     deltax = L/N
-    deltat = 0.001
     xs = np.arange(0, L, deltax)
 
     if which_one==1:
@@ -45,19 +38,34 @@ def one_b(which_one):
 
     sols_next = np.zeros(len(xs))
 
-    overall_solutions = []
+    return (sols_prev, sols, sols_next), xs, deltax
 
+def wave_step_function(all_sols, c, xs, deltax, deltat):
     #the full function 
+    sols_prev, sols, sols_next = all_sols
+    for j,x in enumerate(xs):
+        if j == 0: 
+            sols_next[j]= 0
+        elif j == len(xs)-1:
+            sols_next[j] = 0
+        else:
+            sols_next[j] = np.power(deltat, 2) * np.power(c, 2) * spat_approx_1a(deltax, (sols[j-1], sols[j], sols[j+1])) + 2*sols[j] - sols_prev[j]
+    sols_prev = sols_next.copy()
+    sols = sols_next.copy()
+    return sols_prev, sols, sols_next
+
+def one_b_wrapper(which_one):
+    L = 1
+    N = 100
+    c = 1
+    deltat= 0.001
+    overall_solutions = []
+    soltjes, xs, deltax = initialize_wave(which_one, L, N)
+    overall_solutions.append(soltjes[1])
     for i in range(30000):
-        for j,x in enumerate(xs):
-            if j == 0: 
-                sols_next[j]= 0
-            elif j == len(xs) -1:
-                sols_next[j] = 0
-            else:
-                sols_next[j] = np.power(deltat, 2) * np.power(c, 2) * spat_approx_1a(deltax, time, (sols[j-1], sols[j], sols[j+1]), x, L) + 2*sols[j] - sols_prev[j]
+        soltjes = wave_step_function(soltjes, c, xs, deltax, deltat)
+        # soltjes = soltjes_new[0].copy(), soltjes_new[1].copy(), soltjes_new[2].copy()
         if i%3000 == 0: 
-            overall_solutions.append(sols)
-        sols_prev = sols_next.copy()
-        sols = sols_next.copy()
+            overall_solutions.append(soltjes[1])
     return overall_solutions, xs
+    
--- src/visualizations.py
+++ src/visualizations.py
@@ -1,11 +1,71 @@
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
+import src.solutions as solutions
 
 def visualization_1b(overall_solutions, xs):
-    plt.figure(figsize=(4,4))
-    for k in range(len(overall_solutions)):
-        plt.plot(xs, overall_solutions[k])
+    fig, axs = plt.subplots(1, 3, figsize=(5.5,2.3), sharey=True)
 
-    plt.title("Wave Time-stepping Approximation")
-    plt.xlabel("L")
-    plt.ylabel(r'$\Psi^n$')
+    for j in range(len(overall_solutions)):
+        for k in range(len(overall_solutions[j])):
+            axs[j].plot(xs, overall_solutions[j][k], linewidth=1)
+        axs[j].set_xlabel("L")
+        axs[j].set_title('i'*(j+1))
+   
+    axs[0].set_ylabel(r'$\Psi^n$')
+
+    fig.suptitle("Wave Time-stepping Approximation")
+    plt.tight_layout()
+    fig.subplots_adjust(top=0.8) 
+    plt.savefig("plots/fig_1B.png", dpi=300)
+    plt.show()
+
+
+
+def animate_1c(L, N, c, deltat):
+
+    fig, axs = plt.subplots(1, 3, figsize=(5.3,2.5), sharey=True)
+    # ax.set_title(f"Wave Time-stepping Approximation (frame: 1)")
+    functions = [
+    r"$\sin(2\pi x)$",
+    r"$\sin(5\pi x)$",
+    r"$\sin(5\pi x) \text{ if } \frac{1}{5} < x < \frac{2}{5}, \text{ else } 0$"
+    ]
+    fig.suptitle("Wave Time-Stepping Animation")
+    all_soltjes = []
+    for j in range(3):
+        soltjes, xs, deltax = solutions.initialize_wave(j+1, L, N)
+        all_soltjes.append(soltjes)
+        axs[j].plot(xs, all_soltjes[j][1]) 
+        # axs[j].set_title(functions[j])
+        axs[j].set_title('i'*(j+1))
+        axs[j].set_xlabel("L")
+   
+    axs[0].set_ylabel(r'$\Psi$')
+    
+    plt.tight_layout()  # Prevent overlap
+    fig.subplots_adjust(top=0.8)  # Move suptitle higher
+    plt.pause(1)
+
+    
+
+    def animate(frame):
+        nonlocal all_soltjes
+        for i in range(3):
+            soltjes = all_soltjes[i]
+            axs[i].clear()  # Clear the previous frame
+            axs[i].set_xlim(0, L)  # Set x-axis limits
+            axs[i].set_ylim(-1, 1)  # Set y-axis limits
+            # axs[i].set_title(functions[i])
+            axs[i].set_title('i'*(i+1))
+            for _ in range(100):
+                soltjes = solutions.wave_step_function(soltjes, c, xs, deltax, deltat)
+            all_soltjes[i] = soltjes
+            axs[i].plot(xs, soltjes[1]) 
+            axs[i].set_xlabel("L")
+   
+        axs[0].set_ylabel(r'$\Psi$')
+
+    
+    animation = FuncAnimation(fig, animate, frames=1500, interval=1)
+    animation.save("plots/network_animation10.gif", fps=50,  writer="ffmpeg")
     plt.show()
